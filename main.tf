1b50389d24350d282bd1d45cc6bac7add2d8c889
baezagustin93 <baez.agustin93@gmail.com>
2026-01-22 12:57:26 -0300

    pr1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "example" {
   ami           = "ami-07ff6238b8c7116"
   instance_type = "t2.micro"
   
-  tags {
+  tags = {
     Name = "terraform-example"
   }
 }
--- providers.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  alias = "testing"
-  region = "us-east-1"
-}
